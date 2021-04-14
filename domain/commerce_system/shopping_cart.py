531db0d73221a9e916888a10473fd73e116ba5e2
Gal Dahan <dahanga@post.bgu.ac.il>
2021-04-14 17:33:37 +0300

    shopping cart

--- domain/commerce_system/shopping_cart.py
+++ domain/commerce_system/shopping_cart.py
@@ -6,15 +6,31 @@ from typing import List
 class ShoppingBag:
     def __init__(self, shop: Shop):
         self.shop = shop
+        self.shopping_bag = {}
+        self.shopping_bag_price = 0
 
     def add_product(self, product: Product) -> bool:
-        raise NotImplementedError()
+        if product in self.shopping_bag:
+            self.shopping_bag[product] += 1
+        else:
+            self.shopping_bag[product] = 1
+        self.shopping_bag_price += product.price
+        return True
 
     def remove_product(self, product: Product) -> bool:
-        raise NotImplementedError()
+        if product not in self.shopping_bag:
+            return False
+        if self.shopping_bag[product] == 1:
+            self.shopping_bag.pop(product)
+        else:
+            self.shopping_bag[product] -= 1
+        self.shopping_bag_price -= product.price
+        return True
 
     def remove_all_products(self) -> bool:
-        raise NotImplementedError()
+        self.shopping_bag.clear()
+        self.shopping_bag_price = 0
+        return True
 
 
 class ShoppingCart:
@@ -22,12 +38,15 @@ class ShoppingCart:
         self.shopping_bags: List[ShoppingBag] = []
 
     def add_shopping_bag(self, bag: ShoppingBag) -> bool:
-        raise NotImplementedError()
+        self.shopping_bags += [bag]
+        return True
 
     def remove_shopping_bag(self, bag: ShoppingBag) -> bool:
-        raise NotImplementedError()
+        if bag not in self.shopping_bags:
+            return False
+        self.shopping_bags.remove(bag)
+        return True
 
     def remove_all_shopping_bags(self) -> bool:
-        raise NotImplementedError()
-
-
+        self.shopping_bags.clear()
+        return True
