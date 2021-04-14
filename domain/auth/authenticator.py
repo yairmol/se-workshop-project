a53dd7ab3103b7af34f2134c7b906e3a33bb2cde
aviv7 <62387895+aviv7@users.noreply.github.com>
2021-04-14 14:49:55 +0300

    task2.2 end - adding expire time for tokens

--- domain/auth/authenticator.py
+++ domain/auth/authenticator.py
@@ -1,19 +1,23 @@
+import datetime
 import secrets
 import string
 import threading
+import time
 
 
 class Authenticator:
     alphabet = string.ascii_letters + string.digits
-    tokens_lock = threading.Lock()
+    tokens_ids_lock = threading.Lock()
+    tokens_time_lock = threading.Lock()
 
     def __init__(self):
         self.tokens_ids = {}
+        self.tokens_expire_time = {}
 
     def is_token_exists(self, token):
-        self.tokens_lock.acquire()
+        self.tokens_ids_lock.acquire()
         ret_val = token in self.tokens_ids
-        self.tokens_lock.release()
+        self.tokens_ids_lock.release()
         return ret_val
 
     def generate_token(self):
@@ -23,18 +27,32 @@ class Authenticator:
         return new_token
 
     def add_new_user_token(self, user_id):
-        self.tokens_lock.acquire()
+        self.tokens_ids_lock.acquire()
         new_token = self.generate_token()
         self.tokens_ids[new_token] = user_id
-        self.tokens_lock.release()
+
+        current_date_and_time = datetime.datetime.now()
+        hours = 1
+        hours_added = datetime.timedelta(hours=hours)
+        expire_date = current_date_and_time + hours_added
+        self.tokens_time_lock.acquire()
+        self.tokens_expire_time[new_token] = expire_date
+        self.tokens_time_lock.release()
+
+        self.tokens_ids_lock.release()
         return new_token
 
     # returns the user's id if token exists, -1 if not
     def remove_token(self, token):
         if self.is_token_exists(token):
-            self.tokens_lock.acquire()
+            self.tokens_ids_lock.acquire()
             removed_id = self.tokens_ids[token]
             self.tokens_ids.pop(token)
-            self.tokens_lock.release()
+
+            self.tokens_time_lock.acquire()
+            self.tokens_expire_time.pop(token)
+            self.tokens_time_lock.release()
+
+            self.tokens_ids_lock.release()
             return removed_id
         return -1
