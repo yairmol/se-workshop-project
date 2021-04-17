91575d05f230e9c0236e86d2861e81dfd46a45e7
Yair Molakandov <yair161200@gmail.com>
2021-04-17 22:51:37 +0300

    added get shop_info function

--- domain/commerce_system/product.py
+++ domain/commerce_system/product.py
@@ -1,6 +1,23 @@
+PRODUCT_ID = "product_id"
+PRODUCT_NAME = "product_name"
+PRICE = "price"
+PRODUCT_DESC = "description"
+QUANTITY = "quantity"
+
+
 class Product:
-    def __init__(self, name: str, base_price: float, description: str, quantity: int):
-        self.name = name
-        self.price = base_price
+    def __init__(self, product_id: int, product_name: str, price: float, description: str, quantity: int):
+        self.product_id = product_id
+        self.name = product_name
+        self.price = price
         self.description = description
         self.quantity = quantity
+
+    def to_dict(self):
+        return {
+            PRODUCT_ID: self.product_id,
+            PRODUCT_NAME: self.name,
+            PRODUCT_DESC: self.description,
+            PRICE: self.price,
+            QUANTITY: self.quantity
+        }
