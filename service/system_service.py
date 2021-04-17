ae5fb2dc97b8edbf9c3fdcb3490468b4dca86ee6
aviv7 <62387895+aviv7@users.noreply.github.com>
2021-04-17 14:50:17 +0300

    adding missing functions to service

--- service/system_service.py
+++ service/system_service.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from domain.auth.authenticator import Authenticator
 from domain.commerce_system.commerce_system_facade import CommerceSystemFacade
 
@@ -29,7 +31,7 @@ class SystemService:
             if user_id > 0:
                 self.commerce_system_facade.remove_active_user(user_id)
 
-    def register(self, token: str, username: str, password: str, **more)-> bool:
+    def register(self, token: str, username: str, password: str, **more) -> bool:
         user_id = self.authenticator.get_id_by_token(token)
         try:
             if self.authenticator.is_token_expired(token):
@@ -46,7 +48,7 @@ class SystemService:
             print(e)
             return False
 
-    def login(self, token: str, username: str, password: str)-> bool:
+    def login(self, token: str, username: str, password: str) -> bool:
         user_id = self.authenticator.get_id_by_token(token)
         try:
             if self.authenticator.is_token_expired(token):
@@ -63,13 +65,137 @@ class SystemService:
             print(e)
             return False
 
-    def logout(self, token: str)-> bool:
+    def logout(self, token: str) -> bool:
         user_id = self.authenticator.get_id_by_token(token)
         try:
             if self.authenticator.is_token_expired(token):
                 raise TokenNotValidException("User Token's Expired")
             self.commerce_system_facade.logout(user_id)
             print("LOG: User: " + str(user_id) + " Logged out Successfully")
+            return True
+        except TokenNotValidException as e:
+            print(e)
+            if user_id > 0:
+                self.commerce_system_facade.remove_active_user(user_id)
+            return False
+        except AssertionError as e:
+            print(e)
+            return False
+
+    """NEEDS TO BE CHANGED - HANDLE TRANSPORTING PRODUCT DATA DIFFERENTLY"""
+
+    # def add_product_to_shop(self, user_id: int, shop_id: str, product: Product) -> int:
+    #     pass
+
+    def edit_product_info(self, token: str, shop_id: str, product_id: int, **product_info) -> bool:
+        user_id = self.authenticator.get_id_by_token(token)
+        try:
+            if self.authenticator.is_token_expired(token):
+                raise TokenNotValidException("User Token's Expired")
+            self.commerce_system_facade.edit_product_info(user_id, shop_id, product_id, product_info)
+            print("LOG: User: " + str(user_id) + " Edit product info successfully")
+            return True
+        except TokenNotValidException as e:
+            print(e)
+            if user_id > 0:
+                self.commerce_system_facade.remove_active_user(user_id)
+            return False
+        except AssertionError as e:
+            print(e)
+            return False
+
+    def delete_product(self, token: str, shop_id: str, product_id: str) -> bool:
+        user_id = self.authenticator.get_id_by_token(token)
+        try:
+            if self.authenticator.is_token_expired(token):
+                raise TokenNotValidException("User Token's Expired")
+            self.commerce_system_facade.delete_product(user_id, shop_id, product_id)
+            print("LOG: User: " + str(user_id) + " Delete product info successfully")
+            return True
+        except TokenNotValidException as e:
+            print(e)
+            if user_id > 0:
+                self.commerce_system_facade.remove_active_user(user_id)
+            return False
+        except AssertionError as e:
+            print(e)
+            return False
+
+    def appoint_shop_manager(self, token: str, shop_id: int, username: str, permissions: List[str]) -> bool:
+        user_id = self.authenticator.get_id_by_token(token)
+        try:
+            if self.authenticator.is_token_expired(token):
+                raise TokenNotValidException("User Token's Expired")
+            self.commerce_system_facade.appoint_shop_manager(user_id, shop_id, username, permissions)
+            print("LOG: User: " + str(user_id) + " Appointed shop manager: " + username + " successfully")
+            return True
+        except TokenNotValidException as e:
+            print(e)
+            if user_id > 0:
+                self.commerce_system_facade.remove_active_user(user_id)
+            return False
+        except AssertionError as e:
+            print(e)
+            return False
+
+    def appoint_shop_owner(self, token: str, shop_id: int, username: str) -> bool:
+        user_id = self.authenticator.get_id_by_token(token)
+        try:
+            if self.authenticator.is_token_expired(token):
+                raise TokenNotValidException("User Token's Expired")
+            self.commerce_system_facade.appoint_shop_owner(user_id, shop_id, username)
+            print("LOG: User: " + str(user_id) + " Appointed shop owner: " + username + " successfully")
+            return True
+        except TokenNotValidException as e:
+            print(e)
+            if user_id > 0:
+                self.commerce_system_facade.remove_active_user(user_id)
+            return False
+        except AssertionError as e:
+            print(e)
+            return False
+
+    def edit_manager_permissions(self, token: str, shop_id: int, username: str, permissions: List[str]) -> bool:
+        user_id = self.authenticator.get_id_by_token(token)
+        try:
+            if self.authenticator.is_token_expired(token):
+                raise TokenNotValidException("User Token's Expired")
+            self.commerce_system_facade.edit_manager_permissions(user_id, shop_id, username, permissions)
+            print("LOG: User: " + str(user_id) + " Edited manager: " + username + " permissions successfully")
+            return True
+        except TokenNotValidException as e:
+            print(e)
+            if user_id > 0:
+                self.commerce_system_facade.remove_active_user(user_id)
+            return False
+        except AssertionError as e:
+            print(e)
+            return False
+
+    def un_appoint_manager(self, token: str, shop_id: int, username: str) -> bool:
+        user_id = self.authenticator.get_id_by_token(token)
+        try:
+            if self.authenticator.is_token_expired(token):
+                raise TokenNotValidException("User Token's Expired")
+            self.commerce_system_facade.un_appoint_manager(user_id, shop_id, username)
+            print("LOG: User: " + str(user_id) + " Un appointed manager: " + username + " successfully")
+            return True
+        except TokenNotValidException as e:
+            print(e)
+            if user_id > 0:
+                self.commerce_system_facade.remove_active_user(user_id)
+            return False
+        except AssertionError as e:
+            print(e)
+            return False
+
+    def un_appoint_shop_owner(self, token: str, shop_id: int, username: str):
+        user_id = self.authenticator.get_id_by_token(token)
+        try:
+            if self.authenticator.is_token_expired(token):
+                raise TokenNotValidException("User Token's Expired")
+            self.commerce_system_facade.unappoint_shop_owner(user_id, shop_id, username)
+            print("LOG: User: " + str(user_id) + " Un appointed owner: " + username + " successfully")
             return True
         except TokenNotValidException as e:
             print(e)
