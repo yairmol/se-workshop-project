fe186cbfb7e94413b7298c24072da0de89501de2
Yair Molakandov <yair161200@gmail.com>
2021-05-04 00:00:28 +0300

    added ui cart management (display, edit and remove products)

--- commerce_system_gui/src/api.js
+++ commerce_system_gui/src/api.js
@@ -23,13 +23,27 @@ const routes = {
 
 const base_route = `${host_port}/${routes.base}`;
 
+export const isValidToken = (token) => {
+  const url = "http://127.0.0.1:5000/api/validate_token";
+  return axios({
+    method: "get",
+    url: url,
+    params: {
+      token: token
+    }
+  }).then((res) => {
+    const isvalid = res.data.is_valid;
+    return isvalid;
+  }).catch((err) => alert(`failed to enter the system due to ${err}`))
+}
+
 export const enter = () => {
   const url = "http://127.0.0.1:5000/api/enter";
   return axios({
     method: "post",
     url: url,
   }).then((res) => res.data)
-    .catch((err) => alert(`failed to enter the system due to ${err}`))
+      .catch((err) => alert(`failed to enter the system due to ${err}`))
 }
 
 export const exit = (token) => {
@@ -49,7 +63,6 @@ export const exit = (token) => {
 export const register = (token, user_data) => {
   // const url = `${base_route}/${routes.login}`;
   const url = "http://127.0.0.1:5000/api/register";
-  alert(url);
   return axios({
     method: "post",
     url: url,
@@ -58,13 +71,13 @@ export const register = (token, user_data) => {
       ...user_data
     }
   }).then((res) => res.data.status)
-    .catch((err) => alert(err))
+      .catch((err) => alert(err))
 }
 
 export const login = (token, username, password) => {
   // const url = `${base_route}/${routes.login}`;
   const url = "http://127.0.0.1:5000/api/login";
-  alert(url);
+
   return axios({
     method: "post",
     url: url,
@@ -74,13 +87,13 @@ export const login = (token, username, password) => {
       password: password,
     }
   }).then((result) => result.data.status)
-    .catch((err) => alert(`failed to login due to ${err}`))
+      .catch((err) => alert(`failed to login due to ${err}`))
 }
 
 export const logout = (token) => {
   // const url = `${base_route}/${routes.login}`;
   const url = "http://127.0.0.1:5000/api/logout";
-  alert(url);
+
   return axios({
     method: "put",
     url: url,
@@ -88,6 +101,44 @@ export const logout = (token) => {
       token: token,
     }
   }).then((res) => res.data.status).catch((err) => alert(err))
+}
+
+export const get_cart_info = (token) => {
+  const url = "http://127.0.0.1:5000/api/cart";
+  return axios({
+    url: url,
+    method: "get",
+    params: {
+      token: token
+    },
+  }).then((res) => res.data)
+      .catch((err) => alert(`failed to get cart info due to ${err}`))
+}
+
+export const save_product_to_cart = (token, shop_id, product_id, amount_to_buy) => {
+  const url = `http://127.0.0.1:5000/api/cart/${shop_id}/${product_id}`;
+  return axios({
+    url: url,
+    method: "post",
+    data: {
+      token: token,
+      amount_to_buy: amount_to_buy
+    },
+  }).then((res) => res.data)
+      .catch((err) => alert(`failed to get cart info due to ${err}`))
+}
+
+export const remove_product_from_cart = (token, shop_id, product_id, amount) => {
+  const url = `http://127.0.0.1:5000/api/cart/${shop_id}/${product_id}`;
+  return axios({
+    url: url,
+    method: "delete",
+    data: {
+      token: token,
+      amount: amount,
+    },
+  }).then((res) => res.data)
+      .catch((err) => alert(`failed to remove from cart info due to ${err}`))
 }
 
 export const get_user_transactions = (token) =>
--- commerce_system_gui/src/components/Main.js
+++ commerce_system_gui/src/components/Main.js
@@ -1,17 +1,8 @@
-import {Input} from "@material-ui/core";
-import {useState} from "react";
-import Button from "@material-ui/core/Button";
-
 export const Main = () => {
-  const [query, setQuery] = useState("");
-  const onSearch = () => {
-
-  }
 
   return (
       <div>
-        <Input value={query} onChange={(e) => setQuery(...)}/>
-        <Button onSubmit={}/>
+
       </div>
   );
 };
