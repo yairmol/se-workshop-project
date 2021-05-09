bb8772de58fda12c4bf74a9e1aac092950bcc295
Tommer123 <tommerico2000@gmail.com>
2021-05-09 22:40:38 +0300

    recommit

--- domain/notifications/notifications.py
+++ domain/notifications/notifications.py
@@ -1,11 +1,25 @@
-
 from communication import notifs
 
-def send_notif(client_id, msg):
-    notifs.send_notif(client_id, msg)
+class INotifications:
+    def send_notif(client_id, msg):
+        """Send the msg to an enlisted client with client_id"""
+        raise NotImplementedError()
 
-def send_error(client_id, error):
-    notifs.send_error(client_id, error)
+    def send_error(client_id, error):
+        """Send an error message to an enlisted client with client_id"""
+        raise NotImplementedError()
 
-def send_broadcast(msg):
-    notifs.send_broadcast(msg)
+    def send_broadcast(msg):
+        """Send msg to all enlisted users"""
+        raise NotImplementedError()
+
+
+class Notifications(INotifications):
+    def send_notif(self, client_id, msg):
+        notifs.send_notif(client_id, msg)
+
+    def send_error(self, client_id, error):
+        notifs.send_error(client_id, error)
+
+    def send_broadcast(self, msg):
+        notifs.send_broadcast(msg)
