96e8ebd78ea9dca502b06375768b5541191e90bd
avivalmog <48647789+avivalmog@users.noreply.github.com>
2021-05-21 16:50:48 +0300

    data access layer

---
+++ data_access_layer/init_tables.py
@@ -0,0 +1,67 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import registry
+
+from domain.commerce_system.user import Subscribed
+from domain.commerce_system.shop import Shop
+from domain.commerce_system.product import Product
+
+mapper_registry = registry()
+
+subscribed = Table(
+   'subscribed',
+   mapper_registry.metadata,
+   Column('username', String, primary_key=True),
+   Column('password', String)
+
+
+)
+
+transaction = Table(
+   'transaction',
+   mapper_registry.metadata,
+   Column('id', Integer, primary_key=True),
+)
+
+shop = Table(
+   'shop',
+   mapper_registry.metadata,
+   Column('id', Integer, primary_key=True),
+   Column('name', String),
+   Column('description', String),
+)
+
+product = Table(
+   'products',
+   mapper_registry.metadata,
+   Column('product_id', Integer, primary_key=True),
+   Column('product_name', String),
+   Column('price', Integer),
+   Column('description', String),
+   Column('quantity', Integer),
+   Column('shop_id', Integer),
+)
+categories = Table(
+   'categories',
+   mapper_registry.metadata,
+   Column('category_id', Integer, primary_key=True),
+   Column('category', String),
+)
+
+categories_product_mtm = Table(
+   'product_categories_mtm',
+   mapper_registry.metadata,
+   Column('product_category_id', Integer, primary_key=True),
+   Column(ForeignKey("products.product_id")),
+   Column(ForeignKey("categories.category_id")),
+)
+appointments = Table(
+   'appointments',
+   mapper_registry.metadata,
+   Column(ForeignKey("subscribed.username"), primary_key=True),
+   Column(ForeignKey("shop.id")),
+   Column(ForeignKey("subscribed.username")),
+)
+
+mapper_registry.map_imperatively(Subscribed, subscribed)
+mapper_registry.map_imperatively(Shop, shop)
+mapper_registry.map_imperatively(Product, product)
