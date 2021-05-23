a07723222cb36d16f3298bbc7b8e4fe7c39ba1e4
avivalmog <48647789+avivalmog@users.noreply.github.com>
2021-05-23 16:30:23 +0300

    data access layer - divided responsibility to repositories

---
+++ data_access_layer/shop_repository.py
@@ -0,0 +1,17 @@
+from domain.commerce_system.shop import Shop
+
+
+def save_shop(shop: Shop):
+    pass
+
+
+def save_product():
+    pass
+
+
+def save_discount():
+    pass
+
+
+def save_policy():
+    pass
