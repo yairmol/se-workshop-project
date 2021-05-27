b7c8fe52a1269d0c73f87f970558ded3f1ce7df2
aviv7 <62387895+aviv7@users.noreply.github.com>
2021-05-27 20:13:36 +0300

    added examples for adding Objects to database

--- data_access_layer/subscribed_repository.py
+++ data_access_layer/subscribed_repository.py
@@ -1,9 +1,13 @@
 from domain.commerce_system.appointment import Appointment
 from domain.commerce_system.user import Subscribed
+from init_tables import engine
+from sqlalchemy.orm import Session
 
 
 def save_subscribed(sub: Subscribed):
-    pass
+    with Session(engine) as session:
+        session.add(sub)
+        session.commit()
 
 
 def save_password(password):
