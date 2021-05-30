3a5aa96583f5b5a331d50bc41ce31a41359f492b
Tommer123 <tommerico2000@gmail.com>
2021-05-31 00:00:30 +0300

    add for all except conditions

--- data_access_layer/init_tables.py
+++ data_access_layer/init_tables.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey, DATE, FLOAT
 from sqlalchemy.orm import registry
 
 from domain.commerce_system.transaction import Transaction
@@ -18,15 +18,14 @@ subscribed = Table(
     Column('username', String, primary_key=True),
 )
 
-# TODO: save the transaction's products
 transaction = Table(
     'transaction',
     mapper_registry.metadata,
     Column('id', Integer, primary_key=True),
     Column('username', String),
     Column('shop_id', Integer),
-    # Column('date', ), TODO: how to save date?
-    # Column('price', ), TODO: how to save double/ float
+    Column('date', DATE),
+    Column('price', FLOAT),
 )
 
 shop = Table(
@@ -46,7 +45,7 @@ product = Table(
     Column('price', Integer),
     Column('description', String),
     Column('quantity', Integer),
-    Column('shop_id', Integer),
+    Column('shop_id', Integer, ForeignKey('categories.category_id', ondelete='CASCADE')),
 )
 categories = Table(
     'categories',
@@ -58,17 +57,47 @@ categories = Table(
 categories_product_mtm = Table(
     'product_categories_mtm',
     mapper_registry.metadata,
-    Column('product_category_id', Integer, ForeignKey('categories.category_id', ondelete='CASCADE'), primary_key=True),
-    Column('product_id', Integer, ForeignKey("products.product_id")),
+    Column('product_category_id', Integer, primary_key=True),
+    Column('category_id', Integer, ForeignKey('categories.category_id', ondelete='CASCADE')),
+    Column('product_id', Integer, ForeignKey("products.product_id", ondelete='CASCADE')),
 )
 
-# appointments = Table(
-#     'appointments',
-#     mapper_registry.metadata,
-#     Column(ForeignKey("subscribed.username"), primary_key=True),
-#     Column(ForeignKey("shop.id")),
-#     Column(ForeignKey("subscribed.username")),
-# )
+shop_manager_appointments = Table(
+    'shop_manager_appointments',
+    mapper_registry.metadata,
+    Column('appointee', String, ForeignKey("subscribed.username", ondelete='CASCADE'), primary_key=True),
+    Column('shop_id', Integer, ForeignKey("shop.id")),
+    Column('appointer', String, ForeignKey("subscribed.username", ondelete='CASCADE')),
+    Column('delete_product', Integer),
+    Column('edit_product', Integer),
+    Column('add_product', Integer),
+    Column('discount', Integer),
+    Column('purchase_condition', Integer),
+    Column('get_trans_history', Integer),
+    Column('get_staff_permission', Integer),
+)
+
+shop_owner_appointments = Table(
+    'shop_owner_appointments',
+    mapper_registry.metadata,
+    Column('appointee', String, ForeignKey("subscribed.username", ondelete='CASCADE'), primary_key=True),
+    Column('shop_id', Integer, ForeignKey("shop.id", ondelete='CASCADE')),
+    Column('appointer', String, ForeignKey("subscribed.username", ondelete='CASCADE')),
+
+)
+shopping_cart = Table(
+    'shopping_cart',
+    mapper_registry.metadata,
+    Column('cart_id', Integer, primary_key=True),
+    Column('username', String, ForeignKey("subscribed.username", ondelete='CASCADE'))
+)
+shopping_bag = Table(
+    'shopping_bag',
+    mapper_registry.metadata,
+    Column('shop_id', Integer, ForeignKey("shop.id", ondelete='CASCADE'), primary_key=True),
+    Column('cart_id', Integer, ForeignKey("shopping_cart.cart_id", ondelete='CASCADE')),
+    Column('product_id', Integer, ForeignKey("product.product_id", ondelete='CASCADE'))
+)
 
 mapper_registry.map_imperatively(Subscribed, subscribed)
 mapper_registry.map_imperatively(Shop, shop)
