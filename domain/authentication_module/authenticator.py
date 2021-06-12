b9c3235d5b16eb2fa7ce5ad08f051ffcce53afe3
avivalmog <48647789+avivalmog@users.noreply.github.com>
2021-06-12 18:28:40 +0300

    Full test case of 100 register and login requests

--- domain/authentication_module/authenticator.py
+++ domain/authentication_module/authenticator.py
@@ -6,9 +6,13 @@ import hashlib
 import os
 
 
-def encrypt_password(plaintext_password: str) -> Dict:
-    salt = os.urandom(32)  # A new salt for this user
-    key = hashlib.pbkdf2_hmac('sha256', plaintext_password.encode('utf-8'), salt, 100000)
+def encrypt_password(plaintext_password: str, salt=None) -> Dict:
+    if not salt:
+        salt = os.urandom(32)  # A new salt for this user
+
+    key = int.from_bytes(plaintext_password.encode() + salt, 'big', signed=False) % int(1E10)
+
+    # key = hashlib.pbkdf2_hmac(config[cf.HASH_ALG], plaintext_password.encode('utf-8'), salt, 100000)
     return {'salt': salt, 'key': key}
 
 
@@ -36,5 +40,5 @@ class Authenticator:
 
             salt = self.users_passwords[username]['salt']  # Get the salt
             key = self.users_passwords[username]['key']  # Get the correct key
-            new_key = hashlib.pbkdf2_hmac('sha256', plaintext_password.encode('utf-8'), salt, 100000)
+            new_key = encrypt_password(plaintext_password, salt=salt)["key"]
             assert key == new_key, "Wrong password"
